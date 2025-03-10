17d2ee19d4ce3eb6a46adc1855d1ca3f104f658a
Gideon Ladiyo <alwaysfun0604@gmail.com>
2025-03-11 02:31:03 +0700

    menambahkan class HaloGeneric dengan method SapaUser

---
+++ tpmodul5_2211104022/tpmodul5_2211104022/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class HaloGeneric
+{
+    public void SapaUser<T>(T user)
+    {
+        Console.WriteLine($"Halo user {user}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        HaloGeneric halo = new HaloGeneric();
+        halo.SapaUser("Gideon");
+    } 
+}
