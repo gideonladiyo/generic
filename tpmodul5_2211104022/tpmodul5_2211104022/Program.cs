9af8e119d03f96d5777af6ac7c08a3278d7d809b
Gideon Ladiyo <alwaysfun0604@gmail.com>
2025-03-11 02:43:31 +0700

    menambahkan class DataGeneric

--- tpmodul5_2211104022/tpmodul5_2211104022/Program.cs
+++ tpmodul5_2211104022/tpmodul5_2211104022/Program.cs
@@ -8,11 +8,26 @@ class HaloGeneric
     }
 }
 
+class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void printData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah {data}");
+    }
+}
 class Program
 {
     static void Main()
     {
         HaloGeneric halo = new HaloGeneric();
         halo.SapaUser("Gideon");
+
+        DataGeneric<string> dataNim = new DataGeneric<string>("2211104022");
+        dataNim.printData();
     } 
 }
